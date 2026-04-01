5587b01f9104e4533fbb75083889c362ec86a141
Adriana Capriles Rivera <adricr2004@hotmail.com>
2026-04-01 17:43:29 -0400

    Implement profile edit functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,68 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
   },
 ];
+const cardsContainer = document.querySelector(".cards__list");
+
 initialCards.forEach(function (card) {
-  console.log(card.name);
+  const cardElement = document.createElement("li");
+  cardElement.classList.add("card");
+
+  cardElement.innerHTML = `
+    <img
+      class="card__image"
+      src="${card.link}"
+      alt="${card.name}"
+    />
+    <button
+      class="card__delete-button"
+      type="button"
+    ></button>
+    <div class="card__description">
+      <h2 class="card__title">${card.name}</h2>
+      <button
+        class="card__like-button"
+        type="button"
+      ></button>
+    </div>
+  `;
+
+  cardsContainer.append(cardElement);
 });
+const profileForm = document.querySelector("#edit-profile-form");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-popup");
+const popupClose = editProfileModal.querySelector(".popup__close");
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+editProfileButton.addEventListener("click", handleOpenEditModal);
+popupClose.addEventListener("click", () => closeModal(editProfileModal));
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const inputName = document.querySelector(".popup__input_type_name");
+const inputDescription = document.querySelector(
+  ".popup__input_type_description",
+);
+function fillProfileForm() {
+  inputName.value = profileTitle.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editProfileModal);
+}
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+
+  closeModal(editProfileModal);
+}
+profileForm.addEventListener("submit", handleProfileFormSubmit);
